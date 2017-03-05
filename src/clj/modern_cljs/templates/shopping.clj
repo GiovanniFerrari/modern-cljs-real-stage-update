dd0aa135bee980e4edc187351705efd2a8625f0d
Giovanni Ferrari <ferrarigiovanni555@gmail.com>
2017-03-05 11:04:38 +0100

    updated libraries, arrived to 'dont panic with enlive', no problem for now

--- src/clj/modern_cljs/templates/shopping.clj
+++ src/clj/modern_cljs/templates/shopping.clj
@@ -1,17 +1,19 @@
 (ns modern-cljs.templates.shopping
   (:require [net.cgrand.enlive-html :refer [deftemplate set-attr]]
-            [modern-cljs.remotes :refer [calculate]]))
+            [modern-cljs.remotes :refer [calculate]]
+            ;; added the requirement for the form validators
+            [modern-cljs.shopping.validators :refer [validate-shopping-form]]))
 
-(deftemplate shopping "shopping.html"
-  [quantity price tax discount]
+;; template renamed
+(deftemplate update-shopping-form "shopping.html"
+  [quantity price tax discount errors] ; added errors argument
   [:#quantity] (set-attr :value quantity)
   [:#price] (set-attr :value price)
   [:#tax] (set-attr :value tax)
   [:#discount] (set-attr :value discount)
-  [:#total] (set-attr :value (format "%.2f" (calculate 
-                                             quantity
-                                             price
-                                             tax
-                                             discount))))
+  [:#total] (set-attr :value
+                      (format "%.2f" (double (calculate quantity price tax discount)))))
 
-
+;; new intermediate function
+(defn shopping [q p t d]
+  (update-shopping-form q p t d (validate-shopping-form q p t d)))
